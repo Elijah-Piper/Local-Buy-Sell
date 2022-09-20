535134444ca30f53bef3e1b2165631cfcac40582
Elijah Piper <35882341+Elijah-Piper@users.noreply.github.com>
2022-09-20 07:57:34 -0700

    Initialized Spring Boot API

---
+++ LocalBuySellAPI/src/main/java/com/genspark/LocalBuySellAPI/LocalBuySellApiApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.LocalBuySellAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBuySellApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBuySellApiApplication.class, args);
+	}
+
+}
---
+++ LocalBuySellAPI/src/test/java/com/genspark/LocalBuySellAPI/LocalBuySellApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.LocalBuySellAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBuySellApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
