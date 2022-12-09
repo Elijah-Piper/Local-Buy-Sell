a68e186e8d1f49601f35322be648bb2ed3f20813
Elijah Piper <35882341+Elijah-Piper@users.noreply.github.com>
2022-12-09 09:58:18 -0700

    Set up routing for account editing

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountDetailsComponent } from './account-details/account-details.component';
 import { CreateListingComponent } from './create-listing/create-listing.component';
+import { EditAccountComponent } from './edit-account/edit-account.component';
 import { EditListingComponent } from './edit-listing/edit-listing.component';
 import { ListingDetailsComponent } from './listing-details/listing-details.component';
 import { ListingsComponent } from './listings/listings.component';
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: 'create-listing', component: CreateListingComponent},
   {path: 'account-details', component: AccountDetailsComponent},
   {path: 'edit-listing/:listingId', component: EditListingComponent},
-  {path: 'listing-details/:listingId', component: ListingDetailsComponent}
+  {path: 'listing-details/:listingId', component: ListingDetailsComponent},
+  {path: 'edit-account/:accountId', component: EditAccountComponent}
 ];
 
 @NgModule({
